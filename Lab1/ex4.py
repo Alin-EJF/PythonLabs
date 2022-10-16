a40d405dd7e33191769463157e7b0e66ca4fdbb6
Radu Rares-Aurelian <59961516+raresradua@users.noreply.github.com>
2022-10-16 16:55:28 +0300

    Update ex4.py

--- Lab1/ex4.py
+++ Lab1/ex4.py
@@ -1,3 +1,8 @@
+'''
+aici nu prea tratezi bine corner case-uri, daca iti dau un 'A' la tine imi returneaza '_a' si ar trebui doar 'a', ar trebui sa incerci totusi sa te folosesti
+de faptul ca sunt UpperCamelCase strings, adica incep cu litera mare din start, dupa ideea pe care ai folosit-o e buna
+'''
+
 def camel_to_snake(camel):
     snake = ""
     for c in camel:
