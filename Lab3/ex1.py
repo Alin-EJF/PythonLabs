829c895894e9f35628fa13cc974b17ff24ee86b3
Alin <tdgalin@gmail.com>
2022-10-27 00:18:49 +0300

    aaa

--- Lab3/ex1.py
+++ Lab3/ex1.py
@@ -1,5 +1,3 @@
-#Write a function that receives as parameters two lists a and b and returns a list of sets containing: (a intersected with b, a reunited with b, a - b, b - a)
-
 def set_operations(a, b):
     return [set(a) & set(b), set(a) | set(b), set(a) - set(b), set(b) - set(a)]
 
@@ -7,3 +5,5 @@ a = [1, 2, 3, 4, 5]
 b = [4, 5, 6, 7, 8]
 
 print(set_operations(a, b))
+
+#same as lab2?
