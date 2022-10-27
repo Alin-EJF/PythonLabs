fcf5152922a1f3f1ef396ff947c1e57f4b473689
Alin <tdgalin@gmail.com>
2022-10-28 00:02:43 +0300

    validate_dict

---
+++ Lab3/ex5.py
@@ -0,0 +1,5 @@
+def validate_dict(rules, dictionary):
+    rules_dict = {rule[0]: [rule[1], rule[2], rule[3]] for rule in rules}
+    return all([key in rules and value.startswith(rules_dict[key][1]) and
+                rules_dict[key][2] in value and value.endswith(rules_dict[key][3])
+                for key, value in dictionary.items()])
