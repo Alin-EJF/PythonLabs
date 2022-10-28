893b09aa9de128fd660f0d4a1a15d94d0f48ff0f
Alin <tdgalin@gmail.com>
2022-10-28 09:57:33 +0300

    print

--- Lab3/ex5.py
+++ Lab3/ex5.py
@@ -3,3 +3,5 @@ def validate_dict(rules, dictionary):
     return all([key in rules and value.startswith(rules_dict[key][1]) and
                 rules_dict[key][2] in value and value.endswith(rules_dict[key][3])
                 for key, value in dictionary.items()])
+
+print(validate_dict({("key1", "", "inside", ""), ("key2", "start", "middle", "winter")} , {"key1": "come inside, it's too cold out", "key3": "this is not valid"} ))
